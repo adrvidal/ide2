090beacfa5bce5ebc2088880a6453db5176c60b6
adriv <adrvidal.av@gmail.com>
2019-03-31 18:12:24 +0200

    update C1::m1

--- src/main/java/p1/C1.java
+++ src/main/java/p1/C1.java
@@ -2,7 +2,9 @@ package p1;
 
 //Creamos el paquete p1 y la clase C1 y hacemos un commit del nuevo estado del código
 public class C1 {
-    public void m1(){}
+    public void m1(){
+        int z;
+    }
     public void m2(){}
     public void m3(){}
 }
